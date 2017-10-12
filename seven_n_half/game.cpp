1d3d5eb0bd5e2c14adb0404fc0ee0fc70cef71ac
acrowfliedover <leofanrong@gmail.com>
2017-10-12 14:17:08 -0700

    start betting

--- seven_n_half/game.cpp
+++ seven_n_half/game.cpp
@@ -10,5 +10,14 @@ void play_game();//one complete game
 
 
 void play_game() {
-	
+	int deposit = 1000;//starting cash
+	cout << "Start Game? (Y/N)";
+	while ((cin >>"Y"|| cin >> "y")&& deposit>0) {
+		int bet;
+		do  {
+			cout << "How much do you want to bet?" << endl;
+			cin >> bet;
+		} while(bet <= 0 || bet > deposit)
+
+	}
 }
